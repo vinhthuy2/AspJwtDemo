6f42246c1fe9a5b65c558185e6a376e396223eb8
vinhthuy2 <vinhthuy.tran@zuhlke.com>
2024-05-13 16:47:13 +0700

    add jwthelper + authorization requires

--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
+using System.Text;
+using AspJwtDemo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<ITokenHelper, JwtTokenHelper>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    // .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(
+        options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("supersecret_that_should_be_stored_in_a_secret_manager")),
+                ValidIssuer = "http://localhost:5000",
+                ValidAudience = "http://localhost:5000"
+            };
+        }
+    );
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireClaim("admin", "true"));
+});
 
 var app = builder.Build();
 
@@ -16,29 +52,45 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app
+    .MapGet(
+        "/public-route",
+        () => Results.Ok("Hello, World!")
+    )
+    .WithOpenApi();
+
+app.MapGet(
+        "/private-route",
+        [Authorize]() => Results.Ok("Hello from private route!")
+    )
+    .WithOpenApi();
+
+app.MapGet(
+    "/admin-route-policy",
+    [Authorize("Admin")]() => Results.Ok("Hello from admin route! Protected by policy!")
+);
+
+app.MapGet(
+        "/admin-route-claim",
+        () => Results.Ok("Hello from admin route! Protected by claim!")
+    )
+    .RequireAuthorization()
+    .AddEndpointFilter(
+        async (context, next) =>
+        {
+            if (!context.HttpContext.User.HasClaim("admin", "true"))
+            {
+                return Results.Forbid();
+            }
+
+            return await next(context);
+        }
+    )
+    .WithOpenApi();
+
+app.MapPost("/token", (ITokenHelper jwtHelper, TokenGenerationRequest tokenRequest) => Results.Ok(jwtHelper.GetToken(tokenRequest)));
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
